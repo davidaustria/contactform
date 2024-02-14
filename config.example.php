0ac1c7e6a272a66069a047660e5e915233f671a0
David Austria <davidaustria1@gmail.com>
2024-02-13 18:30:10 -0600

    Changing CharSet from 'iso-8859-1' to 'UTF-8'

--- config.example.php
+++ config.example.php
@@ -37,7 +37,7 @@ define('CONTACTFORM_SMTP_PORT', 587);
 define('CONTACTFORM_SMTP_ENCRYPTION', 'tls');
 
 // Character encoding settings. The default is probably fine for most use cases.
-define('CONTACTFORM_MAIL_CHARSET', 'iso-8859-1'); // Can be: us-ascii, iso-8859-1, utf-8. Default: iso-8859-1.
+define('CONTACTFORM_MAIL_CHARSET', 'UTF-8'); // Can be: us-ascii, iso-8859-1, utf-8. Default: iso-8859-1.
 define('CONTACTFORM_MAIL_ENCODING', '8bit'); // Can be: 7bit, 8bit, base64, binary, quoted-printable. Default: 8bit.
 
 // The language used for error message and the like.
